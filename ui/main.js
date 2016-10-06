01fa7156617a9bb50cf7a004d28e61c9bfa0783a
anupamakpillai <anupamakpillai@gmail.com>
2016-10-06 10:37:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,4 +23,19 @@ button.onclick = function() {
    
 
 };
+//Submit
+var nameInput = document.getElementById('name');
+var submitBtn = document.getElementById('submit');
 
+submitBtn.onclick = function() {
+    var names = ['name1','name2','name3', 'name4'];
+    var list ='';
+    
+    for(var i=0; i<names.length; i++) {
+        list = '<li>' + names[i]  + '</li>';
+    }
+    
+    var ul = document.getElementById('nameList');
+    ul.innerHTML = list;
+};
+}
